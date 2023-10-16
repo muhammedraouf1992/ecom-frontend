7a9510b8b1f35f84456275402fbd073a4ac1468d
muhammed <muhammedraouf92@gmail.com>
2023-10-16 09:38:04 +0300

    fixing errors

--- src/router.jsx
+++ src/router.jsx
@@ -21,111 +21,111 @@ import SingleProduct from "./layout/frontend/pages/SingleProduct";
 import Cart from "./layout/frontend/pages/Cart";
 import Checkout from "./layout/frontend/pages/Checkout";
 import Paypal from "./layout/frontend/pages/Paypal";
-import Orders from "./components/admin/orders/orders";
+import Orders from "./components/admin/orders/Orders";
 import OrderItem from "./components/admin/orders/OrderItem";
 import PrivacyPolicy from "./layout/frontend/pages/PrivacyPolicy";
 import Terms from "./layout/frontend/pages/Terms";
 const router = createBrowserRouter([
-    {
+  {
+    path: "/",
+    element: <FrontendLayout />,
+    children: [
+      {
         path: "/",
-        element: <FrontendLayout />,
-        children: [
-            {
-                path: "/",
-                element: <HomePage />,
-            },
-            {
-                path: "/aboutus",
-                element: <AboutUsPage />,
-            },
-            {
-                path: "/contactus",
-                element: <ContactUsPage />,
-            },
-            {
-                path: "/shop",
-                element: <Collection />,
-            },
-            {
-                path: "/product/:id",
-                element: <ViewProduct />,
-            },
-            {
-                path: "/single-product/:id",
-                element: <SingleProduct />,
-            },
-            {
-                path: "/cart",
-                element: <Cart />,
-            },
-            {
-                path: "/checkout",
-                element: <Checkout />,
-            },
-            {
-                path: "/privacy",
-                element: <PrivacyPolicy />,
-            },
-            {
-                path: "/terms",
-                element: <Terms />,
-            },
-        ],
-    },
-    {
-        path: "/login",
-        element: <Login />,
-    },
-    {
-        path: "/register",
-        element: <Register />,
-    },
-    {
-        path: "admin",
-        element: <MasterLayout />,
-        children: [
-            {
-                index: true,
-                path: "dashboard",
-                element: <Dashboard />,
-            },
-            {
-                path: "profile",
-                element: <Profile />,
-            },
-            {
-                path: "category",
-                element: <Category />,
-            },
-            {
-                path: "category/add",
-                element: <AddCategory />,
-            },
-            {
-                path: "category/edit/:id",
-                element: <EditCategory />,
-            },
-            {
-                path: "product",
-                element: <Products />,
-            },
-            {
-                path: "product/add",
-                element: <AddProduct />,
-            },
-            {
-                path: "product/edit/:id",
-                element: <EditProduct />,
-            },
-            {
-                path: "order/edit/:id",
-                element: <OrderItem />,
-            },
-            {
-                path: "orders",
-                element: <Orders />,
-            },
-        ],
-    },
+        element: <HomePage />,
+      },
+      {
+        path: "/aboutus",
+        element: <AboutUsPage />,
+      },
+      {
+        path: "/contactus",
+        element: <ContactUsPage />,
+      },
+      {
+        path: "/shop",
+        element: <Collection />,
+      },
+      {
+        path: "/product/:id",
+        element: <ViewProduct />,
+      },
+      {
+        path: "/single-product/:id",
+        element: <SingleProduct />,
+      },
+      {
+        path: "/cart",
+        element: <Cart />,
+      },
+      {
+        path: "/checkout",
+        element: <Checkout />,
+      },
+      {
+        path: "/privacy",
+        element: <PrivacyPolicy />,
+      },
+      {
+        path: "/terms",
+        element: <Terms />,
+      },
+    ],
+  },
+  {
+    path: "/login",
+    element: <Login />,
+  },
+  {
+    path: "/register",
+    element: <Register />,
+  },
+  {
+    path: "admin",
+    element: <MasterLayout />,
+    children: [
+      {
+        index: true,
+        path: "dashboard",
+        element: <Dashboard />,
+      },
+      {
+        path: "profile",
+        element: <Profile />,
+      },
+      {
+        path: "category",
+        element: <Category />,
+      },
+      {
+        path: "category/add",
+        element: <AddCategory />,
+      },
+      {
+        path: "category/edit/:id",
+        element: <EditCategory />,
+      },
+      {
+        path: "product",
+        element: <Products />,
+      },
+      {
+        path: "product/add",
+        element: <AddProduct />,
+      },
+      {
+        path: "product/edit/:id",
+        element: <EditProduct />,
+      },
+      {
+        path: "order/edit/:id",
+        element: <OrderItem />,
+      },
+      {
+        path: "orders",
+        element: <Orders />,
+      },
+    ],
+  },
 ]);
 export default router;
